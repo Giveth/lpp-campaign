33d33a92b28fa42d1358fae95b58b6612bdc0409
perissology <perissology@protonmail.com>
2017-10-20 12:22:05 -0700

    minor refactor of contract
    only allow transfers where the paymentState == 'Pledged'
    
    add some comments

--- contracts/LPPCampaign.sol
+++ contracts/LPPCampaign.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.4.13;
 import "../node_modules/liquidpledging/contracts/LiquidPledging.sol";
 import "./Owned.sol";
 
+/// @title LPPCampaign
+/// @author perissology <perissology@protonmail.com>
+/// @notice The LPPCampaign contract is a plugin contract for liquidPledging,
+///  extending the functionality of a liquidPledging project. This contract
+///  prevents withdrawals from any pledges this contract is the owner of.
+///  This contract has 2 roles. The owner and a reviewer. The owner can transfer or cancel
+///  any pledges this contract owns. The reviewer can only cancel the pledges.
+///  If this contract is canceled, all pledges will be rolled back to the previous owner
+///  and will reject all future pledge transfers to the pledgeAdmin represented by this contract
 contract LPPCampaign is Owned {
     uint constant FROM_OWNER = 0;
     uint constant FROM_PROPOSEDPROJECT = 255;
@@ -14,7 +23,13 @@ contract LPPCampaign is Owned {
     address public reviewer;
     address public newReviewer;
 
-    function LPPCampaign(LiquidPledging _liquidPledging, string name, string url, uint64 parentProject, address _reviewer) {
+    function LPPCampaign(
+        LiquidPledging _liquidPledging,
+        string name,
+        string url,
+        uint64 parentProject,
+        address _reviewer
+    ) {
         liquidPledging = _liquidPledging;
         idProject = liquidPledging.addProject(name, url, address(this), parentProject, 0, ILiquidPledgingPlugin(this));
         reviewer = _reviewer;
@@ -40,24 +55,39 @@ contract LPPCampaign is Owned {
         newReviewer = 0;
     }
 
-    function beforeTransfer(uint64 pledgeAdmin, uint64 pledgeFrom, uint64 pledgeTo, uint64 context, uint amount) external returns (uint maxAllowed) {
+    function beforeTransfer(
+        uint64 pledgeAdmin,
+        uint64 pledgeFrom,
+        uint64 pledgeTo,
+        uint64 context,
+        uint amount
+    ) external returns (uint maxAllowed) {
         require(msg.sender == address(liquidPledging));
         var (, , , fromProposedProject , , , ) = liquidPledging.getPledge(pledgeFrom);
         var (, , , , , , toPaymentState ) = liquidPledging.getPledge(pledgeTo);
-        // If I'm the proposed recipient of delegated funds or funds are being directly transferred to me, ensure the I am still active
-        if (   (context == TO_PROPOSEDPROJECT)
-            || (   (context == TO_OWNER)
-                && (fromProposedProject != idProject) && (toPaymentState == LiquidPledgingBase.PaymentState.Pledged)))
+
+        // campaigns can not withdraw funds
+        if ( (context == TO_OWNER) && (toPaymentState != LiquidPledgingBase.PaymentState.Pledged) ) return 0;
+
+        // If this campaign is the proposed recipient of delegated funds or funds are being directly
+        // transferred to me, ensure that the campaign has not been canceled
+        if ( (context == TO_PROPOSEDPROJECT)
+            || ( (context == TO_OWNER) && (fromProposedProject != idProject) ))
         {
             if (isCanceled()) return 0;
         }
         return amount;
     }
 
-    function afterTransfer(uint64 pledgeAdmin, uint64 pledgeFrom, uint64 pledgeTo, uint64 context, uint amount) external {
+    function afterTransfer(
+        uint64 pledgeAdmin,
+        uint64 pledgeFrom,
+        uint64 pledgeTo,
+        uint64 context,
+        uint amount
+    ) external {
         // do nothing
     }
-
 
     function cancelCampaign() public onlyOwnerOrReviewer {
         require( !isCanceled() );
@@ -67,6 +97,7 @@ contract LPPCampaign is Owned {
 
     function transfer(uint64 idSender, uint64 idPledge, uint amount, uint64 idReceiver) public onlyOwner {
       require( !isCanceled() );
+
       liquidPledging.transfer(idSender, idPledge, amount, idReceiver);
     }
 
